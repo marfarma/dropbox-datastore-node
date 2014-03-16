928e9e20f6d7c606b963c14ab8f72195ed58e51b
Pauli Price <pauli.price@gmail.com>
2014-03-16 02:51:35 -0400

    datastoremanager roughed out

--- src/dropbox-datastore-manager.js
+++ src/dropbox-datastore-manager.js
@@ -1,33 +1,107 @@
 "use strict";
 var DropboxDatastore = require('../src/dropbox-datastore-node');
 
-// TODO place extend in a util module
-
-function extend(destination, source) {
-  for (var k in source) {
-    if (source.hasOwnProperty(k)) {
-      destination[k] = source[k];
-    }
-  }
-  return destination; 
-}
-
 function DatastoreManager() {
+  this.app = this.app || process.env.DROPBOX_APP;
+  this.key = this.key || process.env.DROPBOX_KEY;
+  
+  // app owner's token for server wide use
+  // client tokens used for client specific data access
+  // use datastore name of 'server' for server settings tables
+  // and user list
+  
+  this.token = this.key || process.env.DROPBOX_TOKEN;
+  
   this.datastores = {};
+  
+  this.close = function () {
+    for (var ds in this.datastores) {
+      ds.close();
+    }
+  }.bind(this);
+  
+  this.openDefaultDatastore = function (callback) {
+    // datastore.getDefaultDatastore('default',function(data) {
+    //     if (!!data.handle) {
+    //         done();
+    //     } else {
+    //         throw new Error('No data received');
+    //     }
+    // });
+  }.bind(this);
+  
+  this.openDatastore = function (datastoreId, callback) {
+    // datastore.getDatastore(datastoreId, function(data) {
+    //      if (!!data.handle) {
+    //          done();
+    //      } else {
+    //          throw new Error('No data received');
+    //      }
+    // });
+  }.bind(this);
+  
+  this.getOrCreateDatastore = function (datastoreId, callback) {
+    // datastore.getCreateDatastore(datastoreId, function(data) {
+    //      if (!!data.handle) {
+    //         // Assing handle to a temporary 
+    //          global._handle = data.handle;
+    //          done();
+    //      } else {
+    //          throw new Error('No data received');
+    //      }
+    // });
+  }.bind(this);
+  
+  this.createDatastore = function (callback) {
+    // datastore.createDatastore('test',function(data) {
+    //      if (!!data.handle) {
+    //         // Assing handle to a temporary 
+    //          global._handle = data.handle;
+    //          done();
+    //      } else {
+    //          throw new Error('No data received');
+    //      }
+    // });
+  }.bind(this);
+  
+  this.deleteDatastore = function (datastoreId, callback) {
+    // datastore.deleteDatastore(datastoreId,function(data) {
+    //     console.log(data);
+    //     if (!!data.ok) {
+    //         done();
+    //     } else {
+    //         throw new Error('No data received');
+    //     }
+    // });
+  }.bind(this);
+  
+  this.listDatastores = function (callback) {
+      this.ds.listDatastores(callback(data));
+  }.bind(this);
+                  
+  // Events
+  //     datastoreListChanged (Dropbox.Util.EventSource<Dropbox.Datastore.DatastoreListChanged>)
+  //     to listen: DatastoreManager.datastoreListChanged.addListener()
+  // 
+  //     Dropbox.Datastore.DatastoreListChanged
+  //             Array<String> getDatastoreInfos()
+  //                 Returns a list containing current Dropbox.Datastore.DatastoreInfo 
+  //                 objects for all datastores that your app can access.
+  //                 
 
-  // manager isn't initialized until the first datastore access
-  // TODO: check that config options match if existing ds is returned
   this.datastore = function (config) {
-    var id = config.id;
-    var options = config.options;
-    
-    if (typeof this.datastores[config.id] === 'undefined') {
-      var ds = new DropboxDatastore(options);
-      ds._token = config.token;
-      this.datastores[id] = extend({ds: ds},config);
+    // config contains token when request is for a specific user
+    var config.token = config.token || this.token,
+        dsIndex = config.id + config.token;
+        
+    // id is unique for user + app, need token
+    if (typeof this.datastores[dsIndex] === 'undefined') {
+      // get or create datastore using config
+      // config.ds = datastore;
+      this.datastores[dsIndex] = config;
     }
     
-    return this.datastores[id];
+    return this.datastores[dsIndex];
   }.bind(this);
   
 }
